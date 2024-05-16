338acc897ac117717cad9565da29b658e07d789b
keerthan-kumar <keerthankumarroyal@gmail.com>
2024-05-16 20:11:27 +0530

    hi

---
+++ generic_rubiks_cube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Keerthan Kumar on 16-05-2024.
+//
+
+#include "generic_rubiks_cube.h"
---
+++ generic_rubiks_cube.h
@@ -0,0 +1,14 @@
+//
+// Created by Keerthan Kumar on 16-05-2024.
+//
+
+#ifndef PROJECT1_GENERIC_RUBIKS_CUBE_H
+#define PROJECT1_GENERIC_RUBIKS_CUBE_H
+
+
+class generic_rubiks_cube {
+
+};
+
+
+#endif //PROJECT1_GENERIC_RUBIKS_CUBE_H
